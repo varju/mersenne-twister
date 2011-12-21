1de6df277c3278e18c0a9c6ba4f7afdb830c2fb5
Alex Varju <alex@varju.ca>
2011-12-21 13:21:30 -0800

    add commonjs module export

--- mersenne-twister.js
+++ mersenne-twister.js
@@ -194,4 +194,6 @@ MersenneTwister.prototype.genrand_res53 = function() {
   return(a*67108864.0+b)*(1.0/9007199254740992.0); 
 } 
 
+exports.MersenneTwister = MersenneTwister;
+
 /* These real versions are due to Isaku Wada, 2002/01/09 added */
